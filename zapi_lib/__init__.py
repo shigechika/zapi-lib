ce9e2a3478255da0bc16619b88bfec2caedd90e7
Shigechika AIKAWA <shige@aikawa.jp>
2026-06-04 17:15:20 +0900

    chore(main): release 0.2.0

--- zapi_lib/__init__.py
+++ zapi_lib/__init__.py
@@ -9,7 +9,7 @@ from zapi_lib.client import (
     tag_filter,
 )
 
-__version__ = "0.1.0"  # x-release-please-version
+__version__ = "0.2.0"  # x-release-please-version
 
 __all__ = [
     "ZapiClient",
